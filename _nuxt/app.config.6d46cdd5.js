209d62b7303737a5551b492ef80d25bc5f2aa00e
Nicolas SAVOIS <nicolas.savois@gmail.com>
2023-03-07 08:43:45 +0100

    Updates

--- _nuxt/app.config.6d46cdd5.js
+++
@@ -1,1 +0,0 @@
-import{z as u,L as s}from"./entry.d5aa1614.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
---
+++ _nuxt/app.config.87923351.js
@@ -0,0 +1,1 @@
+import{bt as u,bz as s}from"./entry.ac0c3f9c.js";function a(e,n){return u()._useHead(e,n)}const t={};s(t);export{a as u};
