b57119c0934b47c799576961b185813a029584e5
Nicolas SAVOIS <nicolas.savois@gmail.com>
2023-03-23 00:17:16 +0100

    4th show

--- utils/ref-2023/2023.ts
+++ utils/ref-2023/2023.ts
@@ -2,6 +2,7 @@ import { Show, Shows } from '../ref_topchef'
 import { data as data1 } from './2023-1'
 import { data as data2 } from './2023-2'
 import { data as data3 } from './2023-3'
+import { data as data4 } from './2023-4'
 
 export function shows(): Shows {
     return {
@@ -10,6 +11,7 @@ export function shows(): Shows {
             data1,
             data2,
             data3,
+            data4,
         ],
     }
 }
