fcad5abe29a2eb7f842e271b3e323c85b5b4bcbc
Nicolas SAVOIS <nicolas.savois@gmail.com>
2023-04-17 20:59:27 +0200

    status for show 5 6 and 7

---
+++ utils/ref-2023/2023-5.ts
@@ -0,0 +1,97 @@
+import { Show } from "../ref_topchef";
+
+export const data: Show =
+{
+    id: 5,
+    date: '29/03/2023',
+    data: [
+        {
+            name: 'Bleu',
+            leader: 'Etchebest',
+            border: 'border-blue-700',
+            background: 'bg-cyan-200',
+            chefs: [
+                {
+                    name: 'Carla Ferrari', img: 'carla-ferrari-portrait.webp',
+                    dishes: []
+                },
+                {
+                    name: 'Hugo Riboulet', img: 'hugo-riboulet-portrait.webp', dishes: []
+                },
+                {
+                    name: 'Sarika Sor', img: 'sarika-sor-portrait.webp', dishes: []
+                },
+                {
+                    name: 'Albane Aubray', img: 'albane-aubray-portrait.webp', dishes: []
+                },
+            ]
+        },
+        {
+            name: 'Orange',
+            leader: 'Viel',
+            border: 'border-orange-700',
+            background: 'bg-orange-200',
+            chefs: [
+                {
+                    name: 'Jérémie Falissard', img: 'jeremie-falissard-portrait.webp', dishes: []
+                },
+                {
+                    name: 'Mathieu Lagarde', img: 'mathieu-lagarde-portrait.webp', dishes: []
+                },
+                {
+                    name: 'César Lewandowski', img: 'cesar-lewandowski-portrait.webp', dishes: []
+                },
+                {
+                    name: 'Alexandre Marchon', img: 'alexandre-marchon-portrait.webp', dishes: []
+                },
+            ]
+        },
+        {
+            name: 'Violet',
+            leader: 'Pairet',
+            border: 'border-purple-700',
+            background: 'bg-violet-200',
+            chefs: [
+                {
+                    name: 'Jean Covillault', img: 'jean-covillault-portrait.webp', dishes: []
+                },
+                {
+                    name: 'Léo Renusson', img: 'leo-renusson-portrait.webp', dishes: []
+                },
+            ]
+        },
+        {
+            name: 'Rouge',
+            leader: 'Daroze',
+
+            border: 'border-red-700',
+            background: 'bg-red-200',
+            chefs: [
+                {
+                    name: 'Danny Khezzar', img: 'danny-khezzar-portrait.webp', dishes: []
+                },
+            ]
+        },
+        {
+            name: 'Eliminés',
+            leader: '',
+            background: 'bg-gray-200',
+            border: 'border-black',
+            chefs: [
+                { name: 'Bérangère Fagart', img: 'berangere-fagart-portrait.webp', dishes: [] },
+                {
+                    name: 'Miguel Garcia-Herrera', img: 'miguel-garcia-herrera-portrait.webp', dishes: []
+                },
+                {
+                    name: 'Gaston Savina', img: 'gaston-savina-portrait.webp', dishes: []
+                },
+                {
+                    name: 'Jacques Lagarde', img: 'jacques-lagarde-portrait.webp', dishes: []
+                },
+                {
+                    name: 'Victor Blanchet', img: 'victor-blanchet-portrait.webp', dishes: []
+                },
+            ]
+        },
+    ]
+};
--- utils/ref-2023/2023.ts
+++ utils/ref-2023/2023.ts
@@ -3,6 +3,9 @@ import { data as data1 } from './2023-1'
 import { data as data2 } from './2023-2'
 import { data as data3 } from './2023-3'
 import { data as data4 } from './2023-4'
+import { data as data5 } from './2023-5'
+import { data as data6 } from './2023-6'
+import { data as data7 } from './2023-7'
 
 export function shows(): Shows {
     return {
@@ -12,6 +15,9 @@ export function shows(): Shows {
             data2,
             data3,
             data4,
+            data5,
+            data6,
+            data7,
         ],
     }
 }
