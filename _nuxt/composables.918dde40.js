23c54b19f5a60f0e5c09f230af26c4343d7640e5
Nicolas SAVOIS <nicolas.savois@gmail.com>
2024-03-16 20:36:50 +0100

    Updates

---
+++ _nuxt/composables.918dde40.js
@@ -0,0 +1,1 @@
+import{bu as r}from"./entry.3f1a56e7.js";function t(u,e){return r()._useHead(u,e)}export{t as u};
--- _nuxt/composables.c6dca630.js
+++
@@ -1,1 +0,0 @@
-import{bu as r}from"./entry.66be6b7d.js";function t(u,e){return r()._useHead(u,e)}export{t as u};
