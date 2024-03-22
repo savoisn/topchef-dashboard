dca24bd8074777ff520b59149f567abdd39b657d
Nicolas SAVOIS <nicolas.savois@gmail.com>
2024-03-22 19:14:05 +0100

    adding github action workflow

--- pages/test.vue
+++ pages/test.vue
@@ -1,22 +1,22 @@
 <template>
 
     <div class="grid content-center gap-x-8 gap-y-4 grid-cols-2 border-2 " :class="['bg-red-200','border-red-700']">
-    ici
+    ici 1
     </div>
     <div class="grid content-center gap-x-8 gap-y-4 grid-cols-2 border-2 " :class="['bg-cyan-200','border-blue-700']">
-    ici
+    ici 2
     </div>
     <div class="grid content-center gap-x-8 gap-y-4 grid-cols-2 border-2 " :class="['bg-violet-200','border-purple-700']">
-    ici
+    ici 3
     </div>
     <div class="grid content-center gap-x-8 gap-y-4 grid-cols-2 border-2 " :class="['bg-orange-200','border-orange-700']">
-    ici
+    ici 4
     </div>
     <div class="grid content-center gap-x-8 gap-y-4 grid-cols-2 border-2 " :class="['bg-gray-200','border-black']">
-    ici
+    ici 5
     </div>
     <div class="grid content-center gap-x-8 gap-y-4 grid-cols-2 border-2 " :class="['bg-gray-500','border-black']">
-    ici
+    ici 6
     </div>
 </template>
 
